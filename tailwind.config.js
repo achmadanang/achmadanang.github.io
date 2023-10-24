461b021f9ba7b37f77b8fc1e0c5aa44522ceb1a7
anang <anangkancil550@gmail.com>
2023-10-24 20:40:13 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "node_modules/preline/dist/*.js"],
+  content: ["./src/**/*.{html,js}", "node_modules/preline/dist/*.{js}"],
   theme: {
     extend: {},
   },
